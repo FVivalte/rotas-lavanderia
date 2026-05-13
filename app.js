5745a8ff3b4af531442015a0f536408b1b711c85
FVivalte <87857288+FVivalte@users.noreply.github.com>
2026-05-13 19:02:56 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ let rotaGerada = [];
 
 carregarSelecao();
 inicializarDadosSalvos();
+
+window.addEventListener("load", () => {
+
+    const rotaSalva =
+        JSON.parse(localStorage.getItem('rota_salva') || '[]');
+
+    if(rotaSalva.length > 0){
+
+        document.getElementById('view-selecao').style.display = 'none';
+
+        document.getElementById('view-rota-ativa').style.display = 'block';
+
+        renderizarRotaAtiva(rotaSalva);
+
+        iniciarGPS();
+
+    }
+
+});
