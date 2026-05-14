99f2ee88657f512e290026a8961c94b49671d1ad
FVivalte <87857288+FVivalte@users.noreply.github.com>
2026-05-14 15:39:45 -0300

    Atualizar o ui.js

--- ui.js
+++ ui.js
@@ -3,6 +3,34 @@ function toggleCadastro(){
     p.style.display = p.style.display === 'block' ? 'none' : 'block';  
 }
 
+function salvarNovoLocal(){  
+    const nome = document.getElementById('novo-nome').value;  
+    const end = document.getElementById('novo-end').value;  
+    const coords = document.getElementById('novo-coords').value.split(',');  
+    if(!nome || !end || coords.length < 2){  
+        alert("Preencha tudo corretamente. Ex: -22.7490005, -41.9561384");  
+        return;  
+    }  
+    const novo = {  
+        id:Date.now(),  
+        nome,  
+        regiao:"Extra",  
+        endereco:end,  
+        lat:parseFloat(coords[0].trim()),  
+        lon:parseFloat(coords[1].trim()),  
+        custom:true,  
+        prioridade: 99  
+    };  
+    let extras = JSON.parse(localStorage.getItem('locais_extras') || '[]');  
+    extras.push(novo);  
+    localStorage.setItem('locais_extras', JSON.stringify(extras));  
+    document.getElementById('novo-nome').value = '';  
+    document.getElementById('novo-end').value = '';  
+    document.getElementById('novo-coords').value = '';  
+    toggleCadastro();  
+    carregarSelecao();  
+}
+
 function carregarSelecao(){  
     const container = document.getElementById('lista-selecao');  
     container.innerHTML = '';  
