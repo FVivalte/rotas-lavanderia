a829835f35105fe930c5141bdac6ae24cdecf369
FVivalte <87857288+FVivalte@users.noreply.github.com>
2026-05-16 16:07:29 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 window.addEventListener("load", () => {
 
+    if(typeof renderizarLocais === "function"){
+        renderizarLocais();
+    }
+
     carregarRota();
 
     iniciarMonitoramentoGPS();
