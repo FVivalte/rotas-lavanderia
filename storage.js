54da225e168bc7361d1b779e502fea773c639cd5
FVivalte <87857288+FVivalte@users.noreply.github.com>
2026-05-16 12:54:21 -0300

    Atualizar o storage.js

--- storage.js
+++ storage.js
@@ -1,70 +1,37 @@
+function inicializarDadosSalvos() {
 
-const CACHE_NAME = "rotas-cache-v2";
+    const idsSalvos = JSON.parse(
+        localStorage.getItem('rota_salva') || '[]'
+    );
 
-const APP_ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./ui.js",
-  "./rota.js",
-  "./gps.js",
-  "./dados.js",
-  "./storage.js",
-  "./distancia.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+    if (idsSalvos.length > 0) {
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
+        const todos = [
+            ...locaisBase,
+            ...JSON.parse(localStorage.getItem('locais_extras') || '[]')
+        ];
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
+        rotaAtual = todos.filter(
+            l => idsSalvos.includes(l.id)
+        );
+    }
+}
 
-self.addEventListener("fetch", event => {
+function salvarLocalExtra(local) {
+    let extras = JSON.parse(
+        localStorage.getItem('locais_extras') || '[]'
+    );
 
-  if(event.request.method !== "GET"){
-    return;
-  }
+    extras.push(local);
 
-  event.respondWith(
+    localStorage.setItem(
+        'locais_extras',
+        JSON.stringify(extras)
+    );
+}
 
-    caches.match(event.request)
-      .then(cachedResponse => {
-
-        const networkFetch = fetch(event.request)
-          .then(networkResponse => {
-
-            const responseClone =
-              networkResponse.clone();
-
-            caches.open(CACHE_NAME)
-              .then(cache =>
-                cache.put(event.request, responseClone)
-              );
-
-            return networkResponse;
-          })
-          .catch(() => cachedResponse);
-
-        return cachedResponse || networkFetch;
-
-      })
-  );
-});
+function obterLocaisExtras() {
+    return JSON.parse(
+        localStorage.getItem('locais_extras') || '[]'
+    );
+}
