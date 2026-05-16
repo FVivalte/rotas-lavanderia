4c877db40516f70d82e144355ed9f533cf54ce9b
FVivalte <87857288+FVivalte@users.noreply.github.com>
2026-05-16 18:07:45 -0300

    Atualizar o rota-storage.js

--- rota-storage.js
+++ rota-storage.js
@@ -13,6 +13,11 @@ function carregarRota(){
     if(!salva) return;
 
     rotaAtual = JSON.parse(salva);
+    
+    // Adicione esta linha para renderizar a interface
+    if(rotaAtual.ativa){
+        renderizarModoRota();
+    }
 }
 
 function limparRota(){
