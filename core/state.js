3a1a00c48ed78f18d8006699aa829d9a989ad278
FVivalte <87857288+FVivalte@users.noreply.github.com>
2026-05-23 14:26:46 -0300

    Initialize state object in core/state.js

---
+++ core/state.js
@@ -0,0 +1,30 @@
+export const state = {
+
+  activeSet: new Set(),
+
+  routeOrder: [],
+
+  routeReport: [],
+
+  currentIndex: 0,
+
+  userPosition: null,
+
+  speechEnabled: false,
+
+  routingControl: null,
+
+  watchId: null,
+
+  map: null,
+
+  db: null,
+
+  arrivalConfirmed: false,
+
+  mapInitialized: false,
+
+  lastInstruction: '',
+
+  lastHeading: 0
+};
