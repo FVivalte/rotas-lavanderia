52c425c25cdf748d515da8f8bf8ba78f4761809b
FVivalte <87857288+FVivalte@users.noreply.github.com>
2026-05-23 20:22:20 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,33 @@
+// main.js
+
+// ======================
+// CORE
+// ======================
+
+import {
+  initApp
+} from './core/init.js';
+
+
+// ======================
+// EVENTOS
+// ======================
+
+import './events/events.js';
+
+
+// ======================
+// ESTILOS / CONFIG EXTRA
+// ======================
+
+// aqui pode futuramente entrar:
+// import './styles/theme.js';
+// import './services/notifications.js';
+// import './services/backgroundSync.js';
+
+
+// ======================
+// START APP
+// ======================
+
+initApp();
