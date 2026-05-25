31156df890fbaf7cc800246b095edc6a538d69b9
FVivalte <87857288+FVivalte@users.noreply.github.com>
2026-05-25 07:24:47 -0300

    Criar screens.js

---
+++ ui/screens.js
@@ -0,0 +1,28 @@
+import {
+  screenSelect,
+  screenRoute,
+  screenMode,
+  screenReport
+}
+from './elements.js';
+
+const screens = [
+
+  screenSelect,
+  screenRoute,
+  screenMode,
+  screenReport
+
+];
+
+export function showScreen(screen){
+
+  screens.forEach(el=>{
+    el.classList.add('hidden');
+  });
+
+  screen.classList.remove('hidden');
+
+  window.scrollTo(0,0);
+
+}
